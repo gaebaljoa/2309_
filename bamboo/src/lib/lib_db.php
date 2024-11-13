46a4b4e9baef059e0aead5894672d2a46adcfe9b
jiwooo156 <wjdwldn156@naver.com>
2024-11-13 18:18:02 +0900

    241113 list.php 완료

---
+++ bamboo/src/lib/lib_db.php
@@ -0,0 +1,108 @@
+<?php
+// ---------------------------------
+// 함수명   : my_db_conn
+// 기능     : DB Connect
+// 파라미터 : PDO   &$conn
+// 리턴     : boolen
+// ---------------------------------
+function my_db_conn( &$conn ) {
+    $db_host	= "localhost"; // host
+	$db_user	= "root"; // user
+	$db_pw		= "php504"; // password
+	$db_name	= "bamboo"; // DB name
+	$db_charset	= "utf8mb4"; // charset
+	$db_dsn		= "mysql:host=".$db_host.";dbname=".$db_name.";charset=".$db_charset;
+
+    try {
+        $db_options = [
+            PDO::ATTR_EMULATE_PREPARES		=> false // DB의 Prepared Statement 기능을 사용하도록 설정
+			,PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION // 자동으로 PDO Exception을 Throws하도록 설정
+			,PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC // 연상배열로 Fetch를 하도록 설정
+        ];
+
+        $conn = new PDO($db_dsn, $db_user, $db_pw, $db_options);
+        return true;
+    } catch(Exception $e) {
+        echo $e->getMessage();
+        $conn = null;
+        return false;
+    }
+}
+
+
+// ---------------------------------
+// 함수명   : db_destroy_conn
+// 기능     : DB Destroy
+// 파라미터 : PDO   &$conn
+// 리턴     : 없음
+// ---------------------------------
+function db_destroy_conn(&$conn) {
+	$conn = null;
+}
+
+
+// ---------------------------------
+// 함수명   : db_select_boards_cnt
+// 기능     : boards count 조회
+// 파라미터 : PDO		&$conn
+// 리턴     : Int / false
+// ---------------------------------
+function db_select_boards_cnt( &$conn ) {
+    try {
+        $sql = 
+            " SELECT "
+            ."      COUNT(id) as cnt "
+            ." FROM "
+            ."      boards "
+            ." WHERE "
+            ."      delete_flg = 0 "
+            ;
+        $stmt = $conn->query($sql);
+        $result = $stmt->fetchAll();
+        return (int)$result[0]["cnt"];
+    } catch(Exception $e) {
+        return false;
+    }
+}
+
+
+// ---------------------------------
+// 함수명   : db_select_boards_paging
+// 기능     : boards paging 조회
+// 파라미터 : PDO		&$conn
+//			  Array		&$arr_param 쿼리 작성용 배열
+// 리턴     : Array / false
+// ---------------------------------
+function db_select_boards_paging( &$conn, &$arr_param ) {
+    try {
+        $sql = 
+        " SELECT "
+        ."      id "
+        ."      ,title"
+        ."      ,content"
+        ."      ,DATE(create_at) AS create_at"
+        ."  FROM "
+        ."      boards "
+        ."  WHERE "
+        ."      delete_flg = 0 "
+        ."  ORDER BY "
+        ."     id DESC "
+        ."  LIMIT :list_cnt OFFSET :offset "
+        ;
+
+        $arr_ps = [
+            ":list_cnt" => $arr_param["list_cnt"]
+            ,":offset" => $arr_param["offset"]
+        ];
+
+        $stmt = $conn->prepare($sql); // sql 쿼리 준비
+        $stmt->execute($arr_ps); // 플레이스홀드에 값 할당
+        $result = $stmt->fetchAll();
+        return $result;
+    } catch(Exception $e) {
+        echo $e->getMessage();
+        return false;
+    };
+}
+
+?>
---
+++ bamboo/src/list.php
@@ -0,0 +1,121 @@
+<?php
+    define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/bamboo/src/");
+    define("FILE_HEADER", ROOT."header.php");
+    require_once(ROOT."lib/lib_db.php");
+
+    $conn = null;
+    $list_cnt = 5; // 한 페이지에 출력할 게시글 수 
+    $page_num = 1; // 페이지 초기화
+
+    try {
+        // DB 연동
+        if(!my_db_conn($conn)) {
+            throw new Exception("DB ERROR : PDO Instance");
+        }
+        
+        // 페이지를 위한 boards count select 
+        $boards_cnt = db_select_boards_cnt($conn);
+        if($boards_cnt === false) {
+            throw new Exception("DB ERROR : Select count");
+        }
+
+        // 끝 페이지 구하기
+        $max_page_num = ceil($boards_cnt / $list_cnt);
+
+        // 현재 페이지 구하기
+        $page_num = isset($_GET["page"]) ? $_GET["page"] : 1;
+
+        // 오프셋 구하기(몇번째부터 가져올건지)
+        $offset = ($page_num - 1) * $list_cnt;
+
+        // 이전, 다음 페이지 구하기
+        $prev_page_num = $page_num - 1;
+        if($prev_page_num === 0) {
+            $prev_page_num = 1;
+        }
+
+        $next_page_num = $page_num + 1;
+        if($next_page_num > $max_page_num) {
+            $next_page_num = $max_page_num;
+        }
+
+        // 게시글 조회
+        $arr_param = [
+            "list_cnt" => $list_cnt
+            ,"offset" => $offset
+        ];
+
+        $result = db_select_boards_paging($conn, $arr_param);
+        if(!$result) {
+            throw new Exception("DB ERROR : Select boards");
+        }
+    } catch(Exception $e) {
+        echo $e->getMessage();
+        exit;
+    } finally {
+        db_destroy_conn($conn);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/bamboo/src/css/common.css">
+    <title>리스트 페이지</title>
+</head>
+<body>
+    <?php
+        require_once(FILE_HEADER);
+    ?>
+    <main>
+        <div class="insertbtn">
+            <a href="/bamboo/src/insert.php">글 작성</a>
+        </div>
+        <div class="list">
+            <table>
+                <colgroup>
+                    <col width="30%">
+                    <col width="40%">
+                    <col width="30%">
+                </colgroup>
+                <tr>
+                    <th>글 번호</th>
+                    <th>제목</th>
+                    <th>작성일자</th>
+                </tr>
+                <?php
+                    foreach($result as $item) {
+                ?>
+                    <tr>
+                        <td>
+                            <a href="/bamboo/src/detail.php?id=<?php $item["id"] ?>">
+                                <?php echo $item["id"] ?>
+                            </a>
+                        </td>
+                        <td>
+                            <a href="/bamboo/src/detail.php?id=<?php $item["id"] ?>">
+                                <?php echo $item["title"] ?>
+                            </a>
+                        </td>
+                        <td><?php echo $item["create_at"] ?></td>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </table>
+        </div>
+        <div class="pagebtn">
+            <a href="/bamboo/src/list.php?page=<?php echo $prev_page_num ?>">이전</a>
+            <?php 
+                for($i = 1; $i <= $max_page_num; $i++) {
+            ?>
+                <a href="/bamboo/src/list.php?page=<?php echo $i ?>"><?php echo $i ?></a>
+            <?php 
+                } 
+            ?>
+            <a href="/bamboo/src/list.php?page=<?php echo $next_page_num ?>">다음</a>
+        </div>
+    </main>
+</body>
+</html>
